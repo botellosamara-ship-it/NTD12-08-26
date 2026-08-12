512330eb362b80652010c142fc24dc946ed36cf8
ale7516 <vanessaalejandrbarreramartin@gmail.com>
2026-08-12 13:08:59 -0500

    Linea base

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+name = "Juan"
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html', person=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
